c7e66f01c4bc1974c750820122a176817b92818a
medha <medha@MedhaLaptop.dhcp.wustl.edu>
2025-01-21 14:59:27 -0600

    Robot!

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,41 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
+		robot.forward(60);
+		robot.turnLeft(120);
+		robot.forward(60);
+		robot.turnRight(160);
 	}
 
 }
